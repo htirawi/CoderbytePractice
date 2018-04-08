c12d5e639b874f7a25ad5cd4befaa4bcc0420798
Hussein <hmtirawi@gmail.com>
2018-04-08 23:58:02 +0300

    simpleAdding

---
+++ simpleAdding.js
@@ -0,0 +1,11 @@
+function SimpleAdding(num) { 
+var counter = 0;
+  for(var i = 0 ; i <= num;i++){
+      counter+=i;
+  }
+  return counter; 
+         
+}
+   
+// keep this function call here 
+SimpleAdding(readline());
